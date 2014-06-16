796576326e34e6bbd818c06686fd08f242a26023
Kevin Cave <kevin@scarygliders.net>
2014-06-16 16:48:28 +0100

    Added dh-make to list of packages required. Started some thought on a Python-based GUI app replacement for sesconfig.

--- X11rdp-o-matic.sh
+++ X11rdp-o-matic.sh
@@ -5,7 +5,7 @@
 #
 # Version 3.11
 #
-# Version release date : 20140610
+# Version release date : 20140616
 ########################(yyyyMMDD)
 #
 # Will run on Debian-based systems only at the moment. RPM based distros perhaps some time in the future...
@@ -136,7 +136,7 @@ REQUIREDPACKAGES=(build-essential checkinstall automake automake1.9 git
 git-core libssl-dev libpam0g-dev zlib1g-dev libtool libx11-dev libxfixes-dev 
 pkg-config flex bison libxml2-dev intltool xsltproc xutils-dev python-libxml2 
 g++ xutils libfuse-dev wget libxrandr-dev x11proto-* libdrm-dev libpixman-1-dev 
-libgl1-mesa-dev libxkbfile-dev libxfont-dev libpciaccess-dev)
+libgl1-mesa-dev libxkbfile-dev libxfont-dev libpciaccess-dev dh-make)
 
 DIST=`lsb_release -d -s`
 
