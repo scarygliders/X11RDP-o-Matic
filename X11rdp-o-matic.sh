180a06c32a52565d2fe22bdf0c9e8401141ae6cf
Koichiro IWAO <meta@vmeta.jp>
2016-10-17 15:32:36 +0900

    remove dead code

--- X11rdp-o-matic.sh
+++ X11rdp-o-matic.sh
@@ -90,38 +90,6 @@ GIT_USE_HTTPS=true  # Use firewall-friendry https:// instead of git:// to fetch 
   USING_SYSTEMD=false || \
   USING_SYSTEMD=true
 
-# Declare a list of packages required to download sources/compile them...
-REQUIREDPACKAGES=(build-essential checkinstall automake git
-git-core libssl-dev libpam0g-dev zlib1g-dev libtool libx11-dev libxfixes-dev
-pkg-config flex bison libxml2-dev intltool xsltproc xutils-dev python-libxml2
-g++ xutils libfuse-dev libxrandr-dev libdrm-dev libpixman-1-dev
-x11proto-xf86dri-dev
-x11proto-video-dev
-x11proto-resource-dev
-x11proto-dmx-dev
-x11proto-xf86dga-dev
-x11proto-xinerama-dev
-x11proto-render-dev
-x11proto-bigreqs-dev
-x11proto-kb-dev
-x11proto-randr-dev
-x11proto-gl-dev
-x11proto-record-dev
-x11proto-input-dev
-x11proto-fixes-dev
-x11proto-xf86vidmode-dev
-x11proto-xext-dev
-x11proto-scrnsaver-dev
-x11proto-damage-dev
-x11proto-xf86bigfont-dev
-x11proto-composite-dev
-x11proto-core-dev
-x11proto-xcmisc-dev
-x11proto-dri2-dev
-x11proto-fonts-dev
-libgl1-mesa-dev libxkbfile-dev libxfont-dev libpciaccess-dev dh-make gettext
-xfonts-utils)
-
 # libtool binaries are separated to libtool-bin package since Ubuntu 15.04
 # if libtool-bin package exists, add it to REQUIREDPACKAGES
 apt-cache search ^libtool-bin | grep -q libtool-bin && \
